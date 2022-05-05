98d375f57e18ba9499d568bef2fbb921a2e85ef3
Lopez1710 <69539735+Lopez1710@users.noreply.github.com>
2022-05-05 15:32:44 -0600

    Conexión BD

--- Payment_Services/src/main/java/com/unab/DB/ConnectionDB.java
+++ Payment_Services/src/main/java/com/unab/DB/ConnectionDB.java
@@ -4,6 +4,23 @@
  */
 package com.unab.DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class ConnectionDB {
-    
+     private static Connection Con=null;
+    public Connection getCon(){
+        try {
+            String url="jdbc:mysql://localhost:3306/pago_de_facturacion_db";
+            String user="root"; 
+            String password = "root";
+            
+            Con = DriverManager.getConnection(url,user,password);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "error"+e.toString());
+        }
+        return Con;
+    }
 }
