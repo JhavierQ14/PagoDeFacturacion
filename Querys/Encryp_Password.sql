15bf28de65584ee661598d4102aa0002be8679cc
Carlos Henriquez <ha0372032020@unab.edu.sv>
2022-05-06 17:13:23 -0600

    Stored procesure

--- Querys/Encryp_Password.sql
+++ Querys/Encryp_Password.sql
@@ -14,6 +14,26 @@ update pago_de_facturacion_db.user set Password = md5('') where id_User = 0
 
 update pago_de_facturacion_db.user set User_State_id = 0 where id_User = 0;
 
+/*
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_I_USER`(p_user_state_id int, p_user_name varchar(64), p_email varchar(128), p_password varchar(32), p_perfil_image varchar(128))
+BEGIN
+	insert into pago_de_facturacion_db.user(User_State_id,User_Name,Email,Password,perfil_image) values (p_user_state_id,p_user_name,p_email,md5(p_password),p_perfil_image);
+
+END
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_U_USER`(p_user_state_id int, p_user_name varchar(64), p_email varchar(128), pPassword varchar(64), p_perfil_image varchar(128), p_id_user int)
+BEGIN
+	update pago_de_facturacion_db.user set User_State_id = p_user_state_id, User_Name = p_user_name, Email = p_email, Password = md5(pPassword), perfil_image = p_perfil_image where id_User = p_id_user;
+END
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_D_USER`(p_id_user int)
+BEGIN
+	update pago_de_facturacion_db.user set User_State_id = 3 where id_User = p_id_user;
+END
+
+
+*/
+
 
 call pago_de_facturacion_db.SP_I_USER(0, '', '@unab.edu.sv', '', '/image');
 
